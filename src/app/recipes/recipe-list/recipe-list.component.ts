3c8f5e658c5af9a7890ae06f61d5a6be0f08f778
Jean Ducrot <jean.ducrot@interfolio.com>
2018-04-07 07:53:11 -0400

    Progress through 49

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import Recipe from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('First Recipe','This is a test recipe','https://inspirationseek.com/wp-content/uploads/2016/02/Chicken-Cordon-Bleu-Pictures-720x340.jpg')
+  ];
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
